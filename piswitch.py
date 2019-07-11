f43a986792d6ff0f0c14db74335d3301fb5259c1
Shane <shane@shanerice.me>
2019-07-11 01:10:10 -0700

    Added example settings

--- piswitch.py
+++ piswitch.py
@@ -1,6 +1,6 @@
-import json
 import logging
 import os
+import re
 import redis
 import time
 from keyboard import Keyboard
@@ -9,6 +9,7 @@ TWITCH_USER = os.environ.get('TWITCH_USER')
 REDIS_PASS = os.environ.get('REDIS_PASS')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_DB = int(os.environ.get('REDIS_DB', 2))
 
 logger = logging.getLogger('piswitch')
 logger.setLevel(logging.INFO)
@@ -20,7 +21,7 @@ logger.addHandler(fh)
 
 if __name__ == '__main__':
     keyboard = Keyboard()
-    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=2, password=REDIS_PASS)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASS)
     p = r.pubsub()
     p.subscribe(TWITCH_USER)
     while True:
@@ -29,7 +30,8 @@ if __name__ == '__main__':
             logger.info(message)
             if message['type'] == 'message':
                 data = message['data'].decode()
-                message = json.loads(data)
-                logger.info(message)
-                # keyboard.write('G5S1MLBTG\n')
-        time.sleep(2)
+                logger.info(data)
+                level_id = ''.join(re.findall('[a-zA-Z0-9]+', data))
+                logger.info(level_id)
+                keyboard.write('{}\n'.format(level_id))
+        time.sleep(1)
